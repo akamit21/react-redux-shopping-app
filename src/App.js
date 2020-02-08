d6711dfbe32f089187e78902b3fb05c84652c7ee
Amit Kumar <akamit21@outlook.com>
2020-02-08 12:37:21 +0530

    Package Updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import Navbar from "./component/common/Navbar";
 import Footer from "./component/common/Footer";
-import Home from "./component/Home";
-import About from "./component/pages/About";
-import Product from "./component/pages/Product";
-import Items from "./component/pages/Items";
-import Faq from "./component/pages/Faq";
-import Contact from "./component/pages/Contact";
-import Login from "./component/pages/Login";
+
 import "./App.css";
+import Routes from "./routes/Routes";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -29,18 +23,7 @@ export default class App extends React.Component {
     return (
       <React.Fragment>
         <Navbar />
-
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/product" exact component={Product} />
-        <Route path="/product/:for" exact component={Product} />
-        <Route
-          path="/product/:for/:type"
-          render={props => <Items {...props} data={this.state.products} />}
-        />
-        <Route path="/faq" component={Faq} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/login" component={Login} />
+        <Routes />
 
         <div className="container-fluid border-top">
           <Footer />
